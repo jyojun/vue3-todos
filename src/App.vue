daba58918ab3f3a88ad3d0e77976c105be6ff33b
jyojun <parkcode98@gmail.com>
2024-01-28 00:39:07 +0900

    feat: pagination 추가

--- src/App.vue
+++ src/App.vue
@@ -6,93 +6,135 @@
       type="text"
       v-model="searchText"
       placeholder="Search"
+      @keyup.enter="searchTodo"
     />
     <hr />
     <todo-simple-form @add-todo="addTodo" />
+    <div v-if="error !== ''">{{ error }}</div>
 
-    <div v-if="!filteredTodos.length">There is nothing to display.</div>
+    <div v-if="!todos.length">There is nothing to display.</div>
     <todo-list
       @toggle-todo="toggleTodo"
       @delete-todo="deleteTodo"
-      :todos="filteredTodos"
+      :todos="todos"
+    />
+    <hr />
+    <pagination-bar
+      @get-todo="getTodos"
+      :currentPage="currentPage"
+      :numberOfPages="numberOfPages"
     />
   </div>
 </template>
 
 <script>
-import { ref, computed } from "vue";
+import { ref, computed, onMounted, watch } from "vue";
 import TodoSimpleForm from "./components/TodoSimpleForm.vue";
 import TodoList from "./components/TodoList.vue";
+import PaginationBar from "./components/PaginationBar.vue";
+import axios from "axios";
 
 export default {
-  components: { TodoSimpleForm, TodoList },
+  components: { TodoSimpleForm, TodoList, PaginationBar },
   setup() {
     const toggle = ref(false);
-    const items = [
-      {
-        id: 1,
-        subject: "장보기",
-        completed: false,
-      },
-      {
-        id: 2,
-        subject: "청소하기",
-        completed: false,
-      },
-      {
-        id: 3,
-        subject: "메이플 랜드 하기",
-        completed: false,
-      },
-      {
-        id: 4,
-        subject: "코딩하기",
-        completed: false,
-      },
-      {
-        id: 5,
-        subject: "TV 시청",
-        completed: false,
-      },
-    ];
-    const todos = ref([...items]);
+    const error = ref("");
+    const numberOfTodos = ref(0);
+    const limit = 5;
+    const currentPage = ref(1);
+    const todos = ref([]);
+    const searchText = ref("");
 
-    const addTodo = (todo) => {
-      todos.value.push(todo);
+    const numberOfPages = computed(() => {
+      return Math.ceil(numberOfTodos.value / limit);
+    });
+
+    // on mounted
+    onMounted(() => {
+      getTodos();
+    });
+
+    // debounce 적용
+    let timeout = null;
+    watch(searchText, () => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => getTodos(1), 2000); // 검색 delay 추가
+    });
+
+    // methods
+    const getTodos = async (page = currentPage.value) => {
+      currentPage.value = page;
+      try {
+        const res = await axios.get(
+          `http://localhost:3000/todos?_sort=id&_order=desc&subject_like=${searchText.value}&_page=${page}&_limit=${limit}`
+        );
+        numberOfTodos.value = res.headers["x-total-count"];
+        todos.value = res.data;
+      } catch (err) {
+        console.log(err);
+      }
     };
 
-    const toggleTodo = (index) => {
-      todos.value[index].completed = !todos.value[index].completed;
+    const searchTodo = () => {
+      clearTimeout(timeout);
+      getTodos(1);
+    };
+
+    const addTodo = async (todo) => {
+      // json db fetch
+      error.value = "";
+      try {
+        await axios.post("http://localhost:3000/todos", {
+          subject: todo.subject,
+          completed: todo.completed,
+        });
+        getTodos(1);
+      } catch (err) {
+        console.log(err);
+        error.value = "Something went wrong. [ADD]";
+      }
+    };
+
+    const toggleTodo = async (index) => {
+      const id = todos.value[index].id;
+      try {
+        await axios.patch(`http://localhost:3000/todos/${id}`, {
+          completed: !todos.value[index].completed,
+        });
+        todos.value[index].completed = !todos.value[index].completed;
+      } catch (err) {
+        console.log(err);
+        error.value = "Something went wrong. [PATCH]";
+      }
     };
 
     const onToggle = () => {
       toggle.value = !toggle.value;
     };
 
-    const deleteTodo = (index) => {
-      todos.value.splice(index, 1);
+    const deleteTodo = async (index) => {
+      const id = todos.value[index].id;
+      try {
+        await axios.delete(`http://localhost:3000/todos/${id}`);
+        getTodos(1);
+      } catch (err) {
+        console.log(err);
+        error.value = "Something went wrong. [DELETE]";
+      }
     };
 
-    const searchText = ref("");
-    const filteredTodos = computed(() => {
-      if (searchText.value) {
-        return todos.value.filter((todo) =>
-          todo.subject.includes(searchText.value)
-        );
-      }
-
-      return todos.value;
-    });
-
     return {
-      toggle,
       todos,
       addTodo,
+      searchTodo,
       toggleTodo,
       onToggle,
       deleteTodo,
       searchText,
-      filteredTodos,
+      error,
+      getTodos,
+      numberOfPages,
+      currentPage,
     };
   },
 };
---
+++ src/components/PaginationBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav aria-label="Page navigation example">
+    <ul class="pagination">
+      <li v-if="currentPage !== 1" class="page-item">
+        <a
+          style="cursor: pointer"
+          class="page-link"
+          @click="getTodos(currentPage - 1)"
+          >Previous</a
+        >
+      </li>
+      <li
+        v-for="page in numberOfPages"
+        :key="page"
+        class="page-item"
+        :class="currentPage === page ? 'active' : ''"
+      >
+        <a style="cursor: pointer" class="page-link" @click="getTodos(page)">{{
+          page
+        }}</a>
+      </li>
+      <li v-if="currentPage !== numberOfPages" class="page-item">
+        <a
+          style="cursor: pointer"
+          class="page-link"
+          @click="getTodos(currentPage + 1)"
+          >Next</a
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    currentPage: {
+      type: Number,
+      required: true,
+    },
+    numberOfPages: {
+      type: Number,
+      required: true,
+    },
+  },
+  setup(props, { emit }) {
+    const getTodos = (page) => {
+      emit("get-todo", page);
+    };
+
+    return { getTodos };
+  },
+};
+</script>
+
+<style></style>
